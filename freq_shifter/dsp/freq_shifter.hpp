0971bb6cf8f0ff2880cf070995ab3ddeb0aeb8c6
a5632645 <3022602213@qq.com>
2025-08-11 15:19:49 +0800

    testing antalasing frequency shifter

--- freq_shifter/dsp/freq_shifter.hpp
+++ freq_shifter/dsp/freq_shifter.hpp
@@ -3,6 +3,7 @@
 #include "qwqdsp/iir_hilbert.hpp"
 #include "qwqdsp/osciilor/vic_sine_osc.hpp"
 #include "qwqdsp/filter/cheb2_lphp.hpp"
+#include "qwqdsp/filter/complex_halfband.hpp"
 
 namespace dsp {
 class FreqShifter {
@@ -10,17 +11,18 @@ public:
     void Init(float fs) {
         fs_ = fs;
         osc_.Reset(0.5f);
-        cheb2_.Init(fs);
+        // cheb2_.Init(fs);
     }
 
     void Process(std::span<float> left, std::span<float> right) {
         for (auto& s : left) {
-            auto x = cheb2_.Tick(s);
-            s = x;
-            auto a = hilbert_.Tick(x);
+            // auto x = cheb2_.Tick(s);
+            // s = x;
+            auto a = hilbert_.Tick(s);
             osc_.Tick();
             auto b = osc_.GetCpx();
             auto c = a * b;
+            c = filter_.Tick(c);
             s = c.real();
         }
         std::copy(left.begin(), left.end(), right.begin());
@@ -29,20 +31,21 @@ public:
     void SetShift(float freq) {
         osc_.SetFreq(freq, fs_);
         if (freq * freq_ < 0) {
-            cheb2_.Reset();
+            // cheb2_.Reset();
         }
         freq_ = freq;
         if (freq > 0) {
-            cheb2_.MakeLowpass(std::min(22000.0f, fs_ / 2 - freq), -100.0f);
+            // cheb2_.MakeLowpass(std::min(22000.0f, fs_ / 2 - freq), -100.0f);
         }
         else {
-            cheb2_.MakeHighpass(std::max(20.0f, -freq), -100.0f);
+            // cheb2_.MakeHighpass(std::max(20.0f, -freq), -100.0f);
         }
     }
 private:
     qwqdsp::VicSineOsc osc_;
     qwqdsp::IIRHilbertDeeper hilbert_;
-    qwqdsp::Cheb2LowHighpass<8> cheb2_;
+    // qwqdsp::Cheb2LowHighpass<8> cheb2_;
+    qwqdsp::ComplexHalfband<16> filter_;
     float fs_;
     float freq_;
 };
