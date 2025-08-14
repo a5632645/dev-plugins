e0f5236333822371a6c599d6cad5dce0e9a80ee7
a5632645 <3022602213@qq.com>
2025-08-14 23:42:43 +0800

    add elliptic filter design

--- qwqdsp/include/qwqdsp/filter/traditional_design.hpp
+++ qwqdsp/include/qwqdsp/filter/traditional_design.hpp
@@ -57,7 +57,7 @@ struct TraditionalDesign {
         return ret;
     }
 
-    static FilterDesign Chebyshev1(int num_filter, double ripple) {
+    static FilterDesign Chebyshev1(int num_filter, double ripple, bool even_pole_modify) {
         FilterDesign ret{num_filter};
 
         int n = 2 * num_filter;
@@ -67,19 +67,27 @@ struct TraditionalDesign {
         double k_re = std::sinh(A);
         double k_im = std::cosh(A);
         double gain = 1.0;
+        double first_pole = std::cos(pi * (n - 1.0) / (2.0 * n));
+        first_pole = first_pole * first_pole;
         for (int k = 1; k <= num_filter; ++k) {
             double phi = (2.0 * k - 1.0) * pi / (2.0 * n);
-            ret[i].p = std::complex{-std::sin(phi) * k_re, std::cos(phi) * k_im};
+            if (even_pole_modify) {
+                auto pole = std::complex{-std::sin(phi) * k_re, std::cos(phi) * k_im};
+                ret[i].p = std::sqrt((pole * pole + first_pole) / (1.0 - first_pole));
+            }
+            else {
+                ret[i].p = std::complex{-std::sin(phi) * k_re, std::cos(phi) * k_im};
+            }
             gain *= std::norm(ret[i].p);
             ++i;
         }
-        // 切比雪夫I型在DC处的增益, s = 0, 给到第一个滤波器
         gain /= std::sqrt(1.0f + eps * eps);
         ret.k = gain;
 
         return ret;
     }
 
+    // TODO: 偶数极点零点修改
     static FilterDesign Chebyshev2(int num_filter, double ripple) {
         FilterDesign ret{num_filter};
 
@@ -101,18 +109,128 @@ struct TraditionalDesign {
         return ret;
     }
 
-    static FilterDesign Bessel(int num_filter) {
+    struct EllipticHelper {
+    public:
+        static constexpr double kError = 1e-18;
+        static constexpr bool kUseStd = true;
+
+        EllipticHelper(double k0) {
+            while (k0 > kError) {
+                k_.push_back(k0);
+                double kdot = std::sqrt(1.0 - k0 * k0);
+                kdot_.push_back(kdot);
+                k0 = (1.0 - kdot) / (1.0 + kdot);
+            }
+        }
+
+        double CompleteIntegral() {
+            if constexpr (kUseStd) {
+                return std::comp_ellint_1(k_.front());
+            }
+            else {
+                const double a = std::sqrt(1.0 - 1.0e-3);
+                const double k0 = k_.front();
+                if (k0 <= a) {
+                    double km = pi / 2.0;
+                    int m = GetM();
+                    while (m > 0) {
+                        km = km * (1 + k_[m]);
+                        --m;
+                    }
+                    return km;
+                }
+                else {
+                    const double kdot = kdot_.front();
+                    double L = -std::log(kdot / 4.0);
+                    return L + (L - 1.0) * (kdot * kdot) / 2.0;
+                }
+            }
+        }
+
+        std::complex<double> Cd(std::complex<double> u) {
+            auto cdm = std::cos(u * (pi / 2.0));
+            int m = GetM();
+            while (m > 0) {
+                auto invcd = 1.0 / (1 + k_[m]) * (1.0 / cdm + k_[m] * cdm);
+                cdm = 1.0 / invcd;
+                --m;
+            }
+            return cdm;
+        }
+
+        std::complex<double> Sn(std::complex<double> u) {
+            auto snm = std::sin(u * (pi / 2.0));
+            int m = GetM();
+            while (m > 0) {
+                auto invsn = 1.0 / (1 + k_[m]) * (1.0 / snm + k_[m] * snm);
+                snm = 1.0 / invsn;
+                --m;
+            }
+            return snm;
+        }
+
+        std::complex<double> ArcSn(std::complex<double> sn0) {
+            int m = GetM();
+            for (int i = 1; i <= m; ++i) {
+                sn0 = 2.0 * sn0 / ((1.0 + k_[i]) * std::sqrt(1.0 + std::sqrt(1.0 - k_[i - 1] * k_[i - 1] * sn0 * sn0)));
+            }
+            return std::asin(sn0) * (2.0 / pi);
+        }
+
+        int GetM() const {
+            return k_.size() - 1;
+        }
+
+        static double Kdot(double k) {
+            return std::sqrt(1.0 - k * k);
+        }
+    private:
+        std::vector<double> k_;
+        std::vector<double> kdot_;
+    };
+
+    // TODO: 椭圆滤波器的极点有点偏差
+    //       偶数极点零点修改
+    static FilterDesign Elliptic(int num_filter, double amp_passband, double amp_stopband) {
         FilterDesign ret{num_filter};
 
-        double th = pi / (2.0 * num_filter);
-        double k = 1.0;
-        for (int i = 0; i < num_filter; ++i) {
-            double a = std::cos(th * (i + 1.0));
-            double b = std::sin(th * (i + 1.0));
-            ret[i].p = std::complex{-a, b};
-            k *= std::norm(ret[i].p);
+        auto eps_passband = std::sqrt(std::pow(10.0, amp_passband / 10.0) - 1.0);
+        auto eps_stopband = std::sqrt(std::pow(10.0, amp_stopband / 10.0) - 1.0);
+        auto k1 = eps_passband / eps_stopband;
+        int N = 2 * num_filter;
+        auto k1dot = EllipticHelper::Kdot(k1);
+        // ellipdeg get kdot
+        double kdot = 0.0;
+        {
+            int L = num_filter;
+            EllipticHelper helper{k1dot};
+            double f1 = std::pow(k1dot, N);
+            std::complex<double> back{1.0, 0.0};
+            for (int i = 1; i <= L; ++i) {
+                auto ui = (2.0 * i - 1.0) / N;
+                back *= std::pow(helper.Sn(ui), 4.0);
+            }
+            kdot = f1 * std::real(back);
         }
-        ret.k = k;
+        double k = EllipticHelper::Kdot(kdot);
+
+        EllipticHelper helper{k};
+        EllipticHelper helper1{k1};
+        double const k1_complt_int = helper1.CompleteIntegral();
+        auto const v0 = std::complex{0.0, -1.0} * helper1.ArcSn(std::complex{0.0, 1.0} / eps_passband) / (N * k1_complt_int);
+        double gain = 1.0;
+        for (int i = 0; i < num_filter; ++i) {
+            auto& s = ret[i];
+            auto ui = (2.0 * (i + 1) - 1.0) / N;
+            // zero
+            auto epsi = helper.Cd(ui);
+            s.z = std::complex<double>{0.0, 1.0} / (k * epsi);
+            // pole
+            s.p = std::complex{0.0, 1.0} * helper.Cd(ui - v0 * std::complex{0.0, 1.0});
+            gain *= std::norm(s.p) / std::norm(*s.z);
+        }
+        gain /= std::sqrt(1.0 + eps_passband * eps_passband);
+        ret.k = gain;
 
         return ret;
     }
