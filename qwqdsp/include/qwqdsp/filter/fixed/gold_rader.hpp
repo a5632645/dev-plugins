1c57188a4a56163c0182d0392f52f06b69585172
a5632645 <3022602213@qq.com>
2025-08-17 13:19:13 +0800

    add fixed point filter

---
+++ qwqdsp/include/qwqdsp/filter/fixed/gold_rader.hpp
@@ -0,0 +1,73 @@
+#pragma once
+#include <algorithm>
+#include <cstdint>
+#include <limits>
+#include <cmath>
+#include "acc_traits.hpp"
+
+// TODO: add tempalte
+//       fix coeffience calculation
+//       add quantization noise reduce feedback
+namespace qwqdsp::filter::fixed {
+class GoldRader {
+public:
+    using QTYPE = int16_t;
+    using ACCType = AccType<QTYPE>;
+    static constexpr ACCType kMax = std::numeric_limits<QTYPE>::max();
+    static constexpr ACCType kMin = std::numeric_limits<QTYPE>::min();
+    static constexpr QTYPE FRAC_LEN = 15;
+
+    QTYPE Tick(QTYPE x) {
+        ACCType acc = b0_ * x + b1_ * x1_ + b2_ * x2_;
+        x2_ = x1_;
+        x1_ = x;
+        acc += y1_ * rcos_ - y2_ * rsin_;
+        ACCType temp = y2_ * rcos_ + rsin_ * y1_;
+        y2_ = std::clamp(temp >> FRAC_LEN, kMin, kMax);
+        y1_ = std::clamp(acc >> FRAC_LEN, kMin, kMax);
+        return std::clamp(temp >> (FRAC_LEN - bshift_), kMin, kMax);
+    }
+
+    void MakeFromFloat(float b0, float b1, float b2, float pole_radius, float pole_phase) {
+        {
+            float maxb = 0.0f;
+            if (std::abs(b0) > maxb) {
+                maxb = std::abs(b0);
+            }
+            if (std::abs(b1) > maxb) {
+                maxb = std::abs(b1);
+            }
+            if (std::abs(b2) > maxb) {
+                maxb = std::abs(b2);
+            }
+            bshift_ = 0;
+            while (maxb >= 1.0f) {
+                maxb /= 2.0f;
+                ++bshift_;
+            }
+
+            b0_ = (QTYPE)((ACCType)(b0 * (ACCType(1) << FRAC_LEN)) >> bshift_);
+            b1_ = (QTYPE)((ACCType)(b1 * (ACCType(1) << FRAC_LEN)) >> bshift_);
+            b2_ = (QTYPE)((ACCType)(b2 * (ACCType(1) << FRAC_LEN)) >> bshift_);
+        }
+        rcos_ = (QTYPE)((ACCType(1) << FRAC_LEN) * pole_radius * std::cos(pole_phase));
+        rsin_ = (QTYPE)((ACCType(1) << FRAC_LEN) * pole_radius * std::sin(pole_phase));
+    }
+private:
+    QTYPE x1_{};
+    QTYPE x2_{};
+    QTYPE b0_{};
+    QTYPE b1_{};
+    QTYPE b2_{};
+    QTYPE bshift_{};
+
+    QTYPE y1_{};
+    QTYPE y2_{};
+    QTYPE rcos_{};
+    QTYPE rsin_{};
+
+    ACCType quantization_{};
+    ACCType quantization2_{};
+    ACCType mask_{};
+};
+}
--- qwqdsp/include/qwqdsp/filter/rbj.hpp
+++ qwqdsp/include/qwqdsp/filter/rbj.hpp
@@ -6,9 +6,10 @@ struct RBJ {
     float b0;
     float b1;
     float b2;
-    float a0;
     float a1;
     float a2;
+    float pole_radius;
+    float pole_omega;
 
     void Lowpass(float w, float Q) {
         auto a = std::sin(w) / (2 * Q);
@@ -16,9 +17,16 @@ struct RBJ {
         b0 = (1 - cosw) / 2.0f;
         b1 = 1 - cosw;
         b2 = b0;
-        a0 = 1 + a;
         a1 = -2 * cosw;
         a2 = 1 - a;
+        float inva0 = 1.0f / (1 + a);
+        b0 *= inva0;
+        b1 *= inva0;
+        b2 *= inva0;
+        a1 *= inva0;
+        a2 *= inva0;
+        pole_radius = std::sqrt(a2);
+        pole_omega = w;
     }
 
     void Highpass(float w, float Q) {
@@ -27,9 +35,16 @@ struct RBJ {
         b0 = (1 + cosw) / 2.0f;
         b1 = -(1 + cosw);
         b2 = b0;
-        a0 = 1 + a;
         a1 = -2 * cosw;
         a2 = 1 - a;
+        float inva0 = 1.0f / (1 + a);
+        b0 *= inva0;
+        b1 *= inva0;
+        b2 *= inva0;
+        a1 *= inva0;
+        a2 *= inva0;
+        pole_radius = std::sqrt(a2);
+        pole_omega = w;
     }
 
     void Bandpass(float w, float Q) {
@@ -38,9 +53,16 @@ struct RBJ {
         b0 = Q * a;
         b1 = 0;
         b2 = -Q * a;
-        a0 = 1 + a;
         a1 = -2 * cosw;
         a2 = 1 - a;
+        float inva0 = 1.0f / (1 + a);
+        b0 *= inva0;
+        b1 *= inva0;
+        b2 *= inva0;
+        a1 *= inva0;
+        a2 *= inva0;
+        pole_radius = std::sqrt(a2);
+        pole_omega = w;
     }
 
     void Peak(float w, float Q, float g) {
@@ -50,9 +72,16 @@ struct RBJ {
         b0 = 1 + a * A;
         b1 = -2 * cosw;
         b2 = 1 - a * A;
-        a0 = 1 + a / A;
         a1 = -2 * cosw;
         a2 = 1 - a / A;
+        float inva0 = 1.0f / (1 + a / A);
+        b0 *= inva0;
+        b1 *= inva0;
+        b2 *= inva0;
+        a1 *= inva0;
+        a2 *= inva0;
+        pole_radius = std::sqrt(std::abs(a2));
+        pole_omega = w;
     }
 
     void Lowshelf(float w, float Q, float g) {
@@ -63,9 +92,16 @@ struct RBJ {
         b0 = A * ((A + 1) - (A - 1) * cosw + 2 * sqrtA * a);
         b1 = 2 * A * ((A - 1) - (A + 1) * cosw);
         b2 = A * ((A + 1) - (A - 1) * cosw - 2 * sqrtA * a);
-        a0 = (A + 1) + (A - 1) * cosw + 2 * sqrtA * a;
         a1 = -2 * ((A - 1) + (A + 1) * cosw);
         a2 = (A + 1) + (A - 1) * cosw - 2 * sqrtA * a;
+        float inva0 = 1.0f / ((A + 1) + (A - 1) * cosw + 2 * sqrtA * a);
+        b0 *= inva0;
+        b1 *= inva0;
+        b2 *= inva0;
+        a1 *= inva0;
+        a2 *= inva0;
+        pole_radius = std::sqrt(a2);
+        pole_omega = w;
     }
 
     void HighShelf(float w, float Q, float g) {
@@ -76,9 +112,16 @@ struct RBJ {
         b0 = A * ((A + 1) + (A - 1) * cosw + 2 * sqrtA * a);
         b1 = -2 * A * ((A - 1) + (A + 1) * cosw);
         b2 = A * ((A + 1) + (A - 1) * cosw - 2 * sqrtA * a);
-        a0 = (A + 1) - (A - 1) * cosw + 2 * sqrtA * a;
         a1 = 2 * ((A - 1) - (A + 1) * cosw);
         a2 = (A + 1) - (A - 1) * cosw - 2 * sqrtA * a;
+        float inva0 = 1.0f / ((A + 1) - (A - 1) * cosw + 2 * sqrtA * a);
+        b0 *= inva0;
+        b1 *= inva0;
+        b2 *= inva0;
+        a1 *= inva0;
+        a2 *= inva0;
+        pole_radius = std::sqrt(a2);
+        pole_omega = w;
     }
 
     void Notch(float w, float Q) {
@@ -87,9 +130,16 @@ struct RBJ {
         b0 = 1;
         b1 = -2 * cosw;
         b2 = 1;
-        a0 = 1 + a;
         a1 = -2 * cosw;
         a2 = 1 - a;
+        float inva0 = 1.0f / (1 + a);
+        b0 *= inva0;
+        b1 *= inva0;
+        b2 *= inva0;
+        a1 *= inva0;
+        a2 *= inva0;
+        pole_radius = std::sqrt(a2);
+        pole_omega = w;
     }
 
     void Allpass(float w, float Q) {
@@ -98,9 +148,16 @@ struct RBJ {
         b0 = 1 - a;
         b1 = -2 * cosw;
         b2 = 1 + a;
-        a0 = 1 + a;
         a1 = -2 * cosw;
         a2 = 1 - a;
+        float inva0 = 1.0f / (1 + a);
+        b0 *= inva0;
+        b1 *= inva0;
+        b2 *= inva0;
+        a1 *= inva0;
+        a2 *= inva0;
+        pole_radius = std::sqrt(a2);
+        pole_omega = w;
     }
 };
 }
