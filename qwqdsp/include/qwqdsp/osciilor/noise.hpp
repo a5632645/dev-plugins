5d11303fe49e1bf98d628f863060e48379344364
a5632645 <3022602213@qq.com>
2025-09-05 23:45:04 +0800

    更新噪声发射器

--- qwqdsp/include/qwqdsp/osciilor/noise.hpp
+++ qwqdsp/include/qwqdsp/osciilor/noise.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <cstdint>
 #include <limits>
+#include <bit>
 
 namespace qwqdsp::oscillor {
 class WhiteNoise {
@@ -12,7 +13,7 @@ public:
     float Next01() noexcept {
         reg_ *= 1103515245;
         reg_ += 12345;
-        return reg_ / static_cast<float>(std::numeric_limits<uint32_t>::max());
+        return static_cast<float>(reg_) / static_cast<float>(std::numeric_limits<uint32_t>::max());
     }
 
     float Next() noexcept {
@@ -33,11 +34,88 @@ private:
     uint32_t reg_{};
 };
 
-class [[deprecated("not implement")]] PinkNoise {
+/**
+ * @brief Paul Kellet 在 Allan 分析中的改进方法
+ * @ref https://www.firstpr.com.au/dsp/pink-noise/
+ */
+class PinkNoise {
+public:
+    void SetSeed(uint32_t seed) noexcept {
+        white_.SetSeed(seed);
+    }
 
+    float Next() noexcept {
+        float const white = white_.Next();
+        b0_ = 0.99886f * b0_ + white * 0.0555179f;
+        b1_ = 0.99332f * b1_ + white * 0.0750759f;
+        b2_ = 0.96900f * b2_ + white * 0.1538520f;
+        b3_ = 0.86650f * b3_ + white * 0.3104856f;
+        b4_ = 0.55000f * b4_ + white * 0.5329522f;
+        b5_ = -0.7616f * b5_ - white * 0.0168980f;
+        float const pink = b0_ + b1_ + b2_ + b3_ + b4_ + b5_ + b6_ + white * 0.5362f;
+        b6_ = white * 0.115926f;
+        return pink * 0.25f;
+    }
+private:
+    WhiteNoise white_;
+    float b0_{};
+    float b1_{};
+    float b2_{};
+    float b3_{};
+    float b4_{};
+    float b5_{};
+    float b6_{};
 };
 
-class [[deprecated("not implement")]] BrownNoise {
+/**
+ * @brief Voss-McCartney 算法
+ * @ref https://www.firstpr.com.au/dsp/pink-noise/
+ */
+class PinkNoiseHQ {
+public:
+    void SetSeed(uint32_t seed) noexcept {
+        white_.SetSeed(seed);
+        for (auto& s : captrue_noise_) {
+            s = white_.Next();
+        }
+    }
 
+    float Next() noexcept {
+        uint32_t const old = update_phase_;
+        ++update_phase_;
+        uint32_t diff = update_phase_ ^ old;
+        while (diff) {
+            // uint32_t const pos = __builtin_ctzl(diff);
+            auto const pos = std::countr_zero(diff);
+            captrue_noise_[pos] = white_.Next();
+            diff &= ~(static_cast<uint32_t>(1) << pos);
+        }
+
+        float sum{};
+        for (auto const x : captrue_noise_) {
+            sum += x;
+        }
+        return sum / 8.0f;
+    }
+private:
+    WhiteNoise white_;
+    uint32_t update_phase_{};
+    float captrue_noise_[32]{};
+};
+
+class BrownNoise {
+public:
+    void SetSeed(uint32_t seed) noexcept {
+        white_.SetSeed(seed);
+    }
+
+    float Next() noexcept {
+        // 可以用一个更好的积分器
+        latch_ = 0.99f * latch_ + 0.01f * white_.Next();
+        return latch_ * 8.0f;
+    }
+private:
+    WhiteNoise white_;
+    float latch_{};
 };
 } // namespace dsp
--- qwqdsp/playing/playing.cpp
+++ qwqdsp/playing/playing.cpp
@@ -6,13 +6,13 @@
 #include "raylib.h"
 #include "../playing/slider.hpp"
 
-#include "qwqdsp/osciilor/blit.hpp"
+#include "qwqdsp/osciilor/noise.hpp"
 
 static constexpr int kWidth = 500;
 static constexpr int kHeight = 400;
 static constexpr float kFs = 48000.0f;
 
-static qwqdsp::oscillor::Blit dsp;
+static qwqdsp::oscillor::BrownNoise dsp;
 
 static void AudioInputCallback(void* _buffer, unsigned int frames) {
     struct T {
@@ -21,7 +21,7 @@ static void AudioInputCallback(void* _buffer, unsigned int frames) {
     };
     std::span buffer{reinterpret_cast<T*>(_buffer), frames};
     for (auto& s : buffer) {
-        s.l = dsp.Triangle();
+        s.l = dsp.Next();
         s.r = s.l;
     }
 }
@@ -35,63 +35,11 @@ int main(void) {
     SetAudioStreamCallback(stream, AudioInputCallback);
     PlayAudioStream(stream);
     
-    Rectangle dsf_bound;
-    dsf_bound.x = 0;
-    dsf_bound.y = 0;
-    dsf_bound.width = 50;
-    dsf_bound.height = 50;
-
-    Knob w;
-    w.on_value_change = [](float v) {
-        dsp.SetW(v);
-    };
-    dsf_bound.y += dsf_bound.height;
-    w.set_bound(dsf_bound);
-    w.set_range(0.0f, 3.14f, 0.001f, 0.01f);
-    w.set_bg_color(BLACK);
-    w.set_fore_color(RAYWHITE);
-    w.set_title("w");
-    Knob N;
-    N.on_value_change = [](float v) {
-        // dsp.SetN(v);
-    };
-    dsf_bound.y += dsf_bound.height;
-    N.set_bound(dsf_bound);
-    N.set_range(1.0f, 1024.0f, 1.0f, 5.0f);
-    N.set_bg_color(BLACK);
-    N.set_fore_color(RAYWHITE);
-    N.set_title("n");
-    Knob amp;
-    amp.on_value_change = [](float v) {
-        dsp.SetAmp(v);
-    };
-    dsf_bound.y += dsf_bound.height;
-    amp.set_bound(dsf_bound);
-    amp.set_range(0.01f, 0.99f, 0.01f, 0.5f);
-    amp.set_bg_color(BLACK);
-    amp.set_fore_color(RAYWHITE);
-    amp.set_title("amp");
-    Knob pwm;
-    pwm.on_value_change = [](float v) {
-        // dsp.SetPWM(v);
-    };
-    dsf_bound.y += dsf_bound.height;
-    pwm.set_bound(dsf_bound);
-    pwm.set_range(0.01f, 0.99f, 0.01f, 0.5f);
-    pwm.set_bg_color(BLACK);
-    pwm.set_fore_color(RAYWHITE);
-    pwm.set_title("pwm");
-    
     SetTargetFPS(30);
     while (!WindowShouldClose()) {
         BeginDrawing();
         {
             ClearBackground(BLACK);
-
-            w.display();
-            N.display();
-            amp.display();
-            pwm.display();
         }
         EndDrawing();
     }
@@ -100,14 +48,3 @@ int main(void) {
     CloseAudioDevice();
     CloseWindow();
 }
-
-// #include "qwqdsp/misc/integrator.hpp"
-
-// int main() {
-//     float input[512]{1};
-//     float test[512];
-//     qwqdsp::misc::IntegratorTrapezoidal<> inte;
-//     for (size_t i = 0; i < 512; ++i) {
-//         test[i] = inte.Tick(input[i]);
-//     }
-// }
