764d8acd40079cd3bea61df26da8f0d1a4bc1556
a5632645 <3022602213@qq.com>
2025-09-06 20:30:07 +0800

    模板化

--- qwqdsp/include/qwqdsp/osciilor/polyblep.hpp
+++ qwqdsp/include/qwqdsp/osciilor/polyblep.hpp
@@ -6,92 +6,90 @@ namespace qwqdsp::oscillor {
  * @ref https://ccrma.stanford.edu/~juhan/vas.html
  * @ref https://www.researchgate.net/publication/307990687_Rounding_Corners_with_BLAMP
  */
+template<class T = float, bool kUseOrder4 = true>
 class PolyBlep {
 public:
-    static constexpr bool kUseOrder4 = true;
-
-    void SetFreq(float f, float fs) noexcept {
+    void SetFreq(T f, T fs) noexcept {
         phase_inc_ = f / fs;
     }
 
-    void SetPWM(float width) noexcept {
+    void SetPWM(T width) noexcept {
         pwm_ = width;
     }
 
-    float Sawtooth() noexcept {
+    T Sawtooth() noexcept {
         phase_ += phase_inc_;
         phase_ -= std::floor(phase_);
-        float const t = phase_ * 2 - 1;
+        T const t = phase_ * 2 - 1;
         return t - 2 * Blep(phase_, phase_inc_);
     }
 
-    float Sqaure() noexcept {
+    T Sqaure() noexcept {
         phase_ += phase_inc_;
         phase_ -= std::floor(phase_);
-        float const t = phase_ < 0.5f ? 1.0f : -1.0f;
-        float const phase2 = phase_ + 0.5f;
-        float const phase2_wrap = phase2 - std::floor(phase2);
+        T const t = phase_ < static_cast<T>(0.5) ? 1 : -1;
+        T const phase2 = phase_ + static_cast<T>(0.5);
+        T const phase2_wrap = phase2 - std::floor(phase2);
         return t + 2 * (Blep(phase_, phase_inc_) - Blep(phase2_wrap, phase_inc_));
     }
 
-    float PWM() noexcept {
+    T PWM() noexcept {
         phase_ += phase_inc_;
         phase_ -= std::floor(phase_);
-        float const t = phase_ < pwm_ ? 1.0f : -1.0f;
-        float const phase2 = phase_ + 1 - pwm_;
-        float const phase2_wrap = phase2 - std::floor(phase2);
+        T const t = phase_ < pwm_ ? 1 : -1;
+        T const phase2 = phase_ + 1 - pwm_;
+        T const phase2_wrap = phase2 - std::floor(phase2);
         return t + 2 * (Blep(phase_, phase_inc_) - Blep(phase2_wrap, phase_inc_));
     }
 
-    float Triangle() noexcept {
+    T Triangle() noexcept {
         phase_ += phase_inc_;
         phase_ -= std::floor(phase_);
-        float t = 0;
-        if (phase_ < 0.5f) {
+        T t = 0;
+        if (phase_ < static_cast<T>(0.5)) {
             t = 1 - 4 * phase_;
         }
         else {
             t = 4 * phase_ - 3;
         }
-        float const phase2 = phase_ + 0.5f;
-        float const phase2_wrap = phase2 - std::floor(phase2);
+        T const phase2 = phase_ + static_cast<T>(0.5);
+        T const phase2_wrap = phase2 - std::floor(phase2);
         return t + 8 * phase_inc_ * (-Blamp(phase_, phase_inc_) + Blamp(phase2_wrap, phase_inc_));
     }
 private:
-    static constexpr float x2(float x) noexcept {
+    static constexpr T x2(T x) noexcept {
         return x * x;
     }
-    static constexpr float x3(float x) noexcept {
+    static constexpr T x3(T x) noexcept {
         return x2(x) * x;
     }
-    static constexpr float x4(float x) noexcept {
+    static constexpr T x4(T x) noexcept {
         return x2(x) * x2(x);
     }
-    static constexpr float x5(float x) noexcept {
+    static constexpr T x5(T x) noexcept {
         return x2(x) * x3(x);
     }
 
-    static constexpr float Blep(float t, float dt) noexcept {
-        // qwqfixme: 应该乘2
+    static constexpr T Blep(T t, T dt) noexcept {
         if constexpr (kUseOrder4) {
             if (t < dt) {
                 // 0~1
-                float const x = t / dt;
-                return x4(x) / 8 - x3(x) / 3 + 2 * x / 3 - 1.0f / 2;
+                T const x = t / dt;
+                return x4(x) / 8 - x3(x) / 3 + 2 * x / 3 - static_cast<T>(0.5);
             }
             else if (t < 2 * dt) {
                 // 1 ~ 2
-                float const x = t / dt - 1;
-                return -x4(x) / 24 + x3(x) / 6 - x2(x) / 4 + x / 6 - 1.0f / 24;
+                T const x = t / dt - 1;
+                return -x4(x) / 24 + x3(x) / 6 - x2(x) / 4 + x / 6 - static_cast<T>(1.0 / 24);
             }
             else if (t > 1 - dt) {
                 // -1 ~ 0
-                float const x = (t - 1) / dt + 1;
-                return -x4(x) / 8 + x3(x) / 6 + x2(x) / 4 + x / 6 + 1.0f / 24;
+                T const x = (t - 1) / dt + 1;
+                return -x4(x) / 8 + x3(x) / 6 + x2(x) / 4 + x / 6 + static_cast<T>(1.0 / 24);
             }
             else if (t > 1 - dt * 2) {
                 // -2 ~ -1
-                float const x = (t - 1) / dt + 2;
+                T const x = (t - 1) / dt + 2;
                 return x4(x) / 24;
             }
             else {
@@ -101,12 +99,12 @@ private:
         else {
             if (t < dt) {
                 // 0 ~ 1
-                float const x = t / dt;
+                T const x = t / dt;
                 return -x2(x - 1) / 2;
             }
             else if (t > 1 - dt) {
                 // -1 ~ 0
-                float const x = (t - 1) / dt;
+                T const x = (t - 1) / dt;
                 return x2(x + 1) / 2;
             }
             else {
@@ -115,26 +113,26 @@ private:
         }
     }
 
-    static constexpr float Blamp(float t, float dt) noexcept {
+    static constexpr T Blamp(T t, T dt) noexcept {
         if constexpr (kUseOrder4) {
             if (t < dt) {
                 // 0~1
-                float const x = t / dt;
-                return x5(x) / 40 - x4(x) / 12 + x2(x) / 3 - x / 2 + 7.0f / 30;
+                T const x = t / dt;
+                return x5(x) / 40 - x4(x) / 12 + x2(x) / 3 - x / 2 + static_cast<T>(7.0 / 30);
             }
             else if (t < 2 * dt) {
                 // 1 ~ 2
-                float const x = t / dt - 1;
-                return -x5(x) / 120 + x4(x) / 24 - x3(x) / 12 + x2(x) / 12 - x / 24 + 1.0f / 120;
+                T const x = t / dt - 1;
+                return -x5(x) / 120 + x4(x) / 24 - x3(x) / 12 + x2(x) / 12 - x / 24 + static_cast<T>(1.0 / 120);
             }
             else if (t > 1 - dt) {
                 // -1 ~ 0
-                float const x = (t - 1) / dt + 1;
-                return -x5(x) / 40 + x4(x) / 24 + x3(x) / 12 + x2(x) / 12 + x / 24 + 1.0f / 120;
+                T const x = (t - 1) / dt + 1;
+                return -x5(x) / 40 + x4(x) / 24 + x3(x) / 12 + x2(x) / 12 + x / 24 + static_cast<T>(1.0 / 120);
             }
             else if (t > 1 - dt * 2) {
                 // -2 ~ -1
-                float const x = (t - 1) / dt + 2;
+                T const x = (t - 1) / dt + 2;
                 return x5(x) / 120;
             }
             else {
@@ -144,12 +142,12 @@ private:
         else {
             if (t < dt) {
                 // 0 ~ 1
-                float const x = t / dt;
+                T const x = t / dt;
                 return -x3(x - 1) / 6;
             }
             else if (t > 1 - dt) {
                 // -1 ~ 0
-                float const x = (t - 1) / dt;
+                T const x = (t - 1) / dt;
                 return x3(x + 1) / 6;
             }
             else {
@@ -158,8 +156,8 @@ private:
         }
     }
 
-    float phase_{};
-    float phase_inc_{};
-    float pwm_{};
+    T phase_{};
+    T phase_inc_{};
+    T pwm_{};
 };
 }
