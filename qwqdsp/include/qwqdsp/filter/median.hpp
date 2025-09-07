1244b4db51346e5d20187b3ae409f57b06bce4bd
a5632645 <3022602213@qq.com>
2025-09-07 10:50:46 +0800

    更新中位数滤波器

--- qwqdsp/include/qwqdsp/filter/median.hpp
+++ qwqdsp/include/qwqdsp/filter/median.hpp
@@ -1,25 +1,27 @@
 /*
-MIT License
-
-Copyright (c) 2018 Alexandru Bogdan
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+ * based on https://github.com/accabog/MedianFilter
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2018 Alexandru Bogdan
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
 */
 
 #pragma once
@@ -28,10 +30,7 @@ SOFTWARE.
 #include <array>
 
 namespace qwqdsp::filter {
-/**
- * @ref https://github.com/accabog/MedianFilter
- */
-template<class T> requires std::is_arithmetic_v<T>
+template<class T> requires std::is_trivial_v<T>
 class MedianDynamic {
 public:
     void Init(size_t window_size) {
@@ -109,10 +108,12 @@ public:
 
 
     /**
-     * @tparam Func bool less_than(T const& a, T const & b)
+     * @tparam Func std::partial_ordering compare(T const& a, T const & b)
      */
-    template<class Func>
-    T Tick(T x, Func&& less_func) noexcept(noexcept(less_func(std::declval<T>(), std::declval<T>()))) {
+    template<class Func> requires requires (T const& a, T const& b, Func comparator) {
+        {comparator(a, b)} -> std::same_as<std::partial_ordering>;
+    }
+    T Tick(T x, Func&& compare) noexcept(noexcept(compare(std::declval<T>(), std::declval<T>()))) {
         [[unlikely]]
         if (first_init_) {
             first_init_ = false;
@@ -126,7 +127,7 @@ public:
             age_head_ = value_head_->next_value;
         }
 
-        if((age_head_ == median_head_) || (age_head_->value > median_head_->value)) {
+        if((age_head_ == median_head_) || (compare(age_head_->value, median_head_->value) == std::partial_ordering::greater)) {
             median_head_ = median_head_->prev_value;
         }
 
@@ -140,7 +141,7 @@ public:
         auto* it = value_head_;
         size_t i = 0;
         for(;i < buffer_.size() - 1; i++) {
-            if(less_func(x, it->value))
+            if(compare(x, it->value) == std::partial_ordering::less)
             {
                 if(i == 0)
                 {
@@ -181,7 +182,7 @@ template<class T, size_t kWindowSize>
 class Median {
 public:
     static_assert(kWindowSize > 2 && kWindowSize % 2 == 1);
-    static_assert(std::is_arithmetic<T>::value);
+    static_assert(std::is_trivial_v<T>);
 
     Median() {
         Reset();
@@ -203,10 +204,12 @@ public:
     }
 
     /**
-     * @tparam Func bool less_than(T const& a, T const & b)
+     * @tparam Func std::partial_ordering compare(T const& a, T const & b)
      */
-    template<class Func>
-    T Tick(T x, Func&& less_func) noexcept(noexcept(less_func(std::declval<T>(), std::declval<T>()))) {
+    template<class Func> requires requires (T const& a, T const& b, Func comparator) {
+        {comparator(a, b)} -> std::same_as<std::partial_ordering>;
+    }
+    T Tick(T x, Func&& compare) noexcept(noexcept(compare(std::declval<T>(), std::declval<T>()))) {
         [[unlikely]]
         if (first_init_) {
             first_init_ = false;
@@ -220,7 +223,7 @@ public:
             age_head_ = value_head_->next_value;
         }
 
-        if((age_head_ == median_head_) || (age_head_->value > median_head_->value)) {
+        if((age_head_ == median_head_) || (compare(age_head_->value, median_head_->value) == std::partial_ordering::greater)) {
             median_head_ = median_head_->prev_value;
         }
 
@@ -234,7 +237,7 @@ public:
         auto* it = value_head_;
         size_t i = 0;
         for(;i < buffer_.size() - 1; i++) {
-            if(less_func(x, it->value))
+            if(compare(x, it->value) == std::partial_ordering::less)
             {
                 if(i == 0)
                 {
